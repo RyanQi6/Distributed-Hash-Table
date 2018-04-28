97e19f97a076af15d08d65704145750f3d0042ee
sly1061101 <sly1061101@gmail.com>
2018-04-28 16:58:51 -0500

    Modify cpp

--- out/production/Distributed-Hash-Table/main.cpp
+++ out/production/Distributed-Hash-Table/main.cpp
@@ -10,6 +10,9 @@ int main(int argc, char** argv) {
    if(fork() == 0){ 
 	 	string command("java main slave ");
 		command = command + argv[1];
+		command = command + " 127.0.0.1 ";
+		command = command + " " + to_string((3000 + stoi(argv[1])));
+		command = command + " 2999 3000";
 		system(command.c_str());
         exit(0);
     }
